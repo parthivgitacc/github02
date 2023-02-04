15ffa9d59d6f3b3aca398f7910d9d1abcc497ed7
parthivgitacc <parthieswar17@gmail.com>
2023-02-04 11:54:31 +0530

    Gitjava

---
+++ src/org/number/ReverseNumber.java
@@ -0,0 +1,28 @@
+package org.number;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	
+	 public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int num = input.nextInt();
+	        int reverse = 0;
+	        int original = num;
+	        
+	        while (num != 0) {
+	            int digit = num % 10;
+	            reverse = reverse * 10 + digit;
+	            num /= 10;
+	        }
+	        
+	        if (original == reverse) {
+	            System.out.println(original + " is a reverse number.");
+	        } else {
+	            System.out.println(original + " is not a reverse number.");
+	        }
+	    }
+	}
+
